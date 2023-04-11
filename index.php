150f3a7724d26d7847637d1099825decdc2ec6d8
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-04-11 09:57:59 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        this is ny first
+    </div>
+    <?php
+echo"hello again";
+$var1=45;
+$var2=5;
+echo $var1;
+echo $var2;
+echo$var1+$var2
+
+
+
+    ?>
+
+    <?php
+//operators in php
+echo"the value of var1 and var2 is ";
+echo"<br>";
+echo $var1+$var2
+
+    ?>
+
+</body>
+</html>
