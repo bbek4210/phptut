9a1245f3ba066aee13813a70dd497220a25184b0
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-04-14 19:50:14 +0545

    tra

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
-$server="localhost";
-$username="root";
-$password="";
+$server = "localhost";
+$username = "root";
+$password = "";
 
-$con= mysqli_connect($server,$username,$password);
+$con = mysqli_connect($server, $username, $password);
 
-if(!$con){
+if (!$con) {
     die("Could not connect to database failed: " . mysqli_connect_error());
 }
-echo"success connection to the database";
+// echo "success connection to the database";
+
+$name=$_POST['name'];
+$gender=$_POST['gender'];
+$age=$_POST['age'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$other=$_POST['other'];
+
+
+$sql="INSERT INTO `travel` (`name`, `age`, `gender`, `email`, `phone`, `other`, `date`) VALUES ('bibek', '21', 'male', 'bibej@gmail.com', '9814422779', 'hhh', current_timestamp());"
