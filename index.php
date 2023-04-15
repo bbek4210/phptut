46b7479751d032b0b8ee3e5ec5c6a7bcf40209b6
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-04-15 22:27:00 +0545

    travel

--- index.php
+++ index.php
@@ -10,10 +10,12 @@ if (!$con) {
 }
 // echo "success connection to the database";
 
-$name=$_POST['name'];
-$gender=$_POST['gender'];
-$age=$_POST['age'];
-$email=$_POST['email'];
-$phone=$_POST['phone'];
-$other=$_POST['other'];
+$name = $_POST['name'];
+$gender = $_POST['gender'];
+$age = $_POST['age'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$other = $_POST['other'];
 
+
+$sql = "INSERT INTO `travel` (`name`, `age`, `gender`, `email`, `phone`, `other`, `date`) VALUES ('bibek', '21', 'male', 'bibej@gmail.com', '9814422779', 'hhh', current_timestamp());"
